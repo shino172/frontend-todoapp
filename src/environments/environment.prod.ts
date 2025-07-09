e4789d0c3bb38fcf358c49b3ad3be545099c2b87
shino172 <anhhao24062004@gmail.com>
2025-07-09 13:46:56 +0700

    update environments

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  api: 'https://backend-todoapp-fuz1.onrender.com'
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  api: 'https://backend-todoapp-fuz1.onrender.com'
+}
